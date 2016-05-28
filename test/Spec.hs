db4951ddee8997ae8504ba7622a6991341b4eff1
Liu Enze <liuenze6516@gmail.com>
2016-05-28 09:43:15 +0800

    add test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,17 @@
+import Test.Hspec
+import Test.QuickCheck
+
+import Intro01
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "luhn" $ do
+    it "luhn 5594589764218858 = True " $ do
+      luhn 5594589764218858 `shouldBe` True
+    it "luhn 1234567898765432 = False" $ do
+      luhn 1234567898765432 `shouldBe` False
+--    it "luhn 1234567898765432 = False" $ property $
+--      \str -> strip str == strip (strip str)
